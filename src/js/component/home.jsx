1e22771dffeead4f8fa81542aa1a34791dbe8fb8
Michell D'Enjoy <166136472+michelldenjoy@users.noreply.github.com>
2024-07-18 13:51:45 +0000

    actualizacion de todos los componentes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,76 +1,39 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-// importacion del navbar
 import NavBar from "./navbar/navbar";
-
-// importacion del jumbotron
 import Jumbotron from "./jumbotron/jumbotron";
-//importacion de las cartas
-import Card from "./cards/card";
+import Cards from "./card/cards"
+import Footer from "./footer/footer";
 
-//create your first component
 const Home = () => {
-	const datosCartas = [
-		{
-			id: 1,
-			imagen: "https://placehold.co/600x400",
-			titulo: "Carta 1",
-			texto: "texto1",
-			url: "#",
-		},
-		{
-			id: 2,
-			imagen: "https://placehold.co/600x400",
-			titulo: "Carta 2",
-			texto: "texto2",
-			url: "#",
-		},
-		{
-			id: 3,
-			imagen: "https://placehold.co/600x400",
-			titulo: "Carta 3",
-			texto: "texto3",
-			url: "#",
-		},
-		{
-			id: 4,
-			imagen: "https://placehold.co/600x400",
-			titulo: "Carta 4",
-			texto: "texto4",
-			url: "#",
-		}
-	]
-
 	return (
-		<div>
-			<div>
+		<>
+			<div> {/*barra de navegacion*/}
 				<NavBar />
 			</div>
-			<main>
-				<Jumbotron />
-				<div className="container">
-					<div className="row">
-						{datosCartas.map((datoCarta) => (
-							<div className="col-sm-12 col-md-3">
-								<Card
-									key={datoCarta.id}
-									datos={datoCarta}
-								/>
-							</div>
-						))}
-					</div>
 
+			<main className="container">
+				<Jumbotron />
+				<div className="row"> {/*cartas repetidas*/}
+					<div className="col-sm-1 col-md-3">
+						<Cards img={"https://placehold.co/200"} titulo={"TITULO"} text={"Algun texto aqui"} enlace={"www.michell.com"} />
+					</div>
+					<div className="col-sm-1 col-md-3">
+						<Cards img={"https://placehold.co/200"} titulo={"TITULO"} text={"Algun texto aqui"} enlace={"www.michell.com"} />
+					</div>
+					<div className="col-sm-1 col-md-3">
+						<Cards img={"https://placehold.co/200"} titulo={"TITULO"} text={"Algun texto aqui"} enlace={"www.michell.com"} />
+					</div>
+					<div className="col-sm-1 col-md-3">
+						<Cards img={"https://placehold.co/200"} titulo={"TITULO"} text={"Algun texto aqui"} enlace={"www.michell.com"} />
+					</div> {/*hasta aqui estan las cartas repetidas*/}
 				</div>
 			</main>
 			<footer>
-
+				<Footer />
 			</footer>
-		</div>
+
+		</>
+
 	);
-
-
 };
-
 export default Home;
--- src/js/component/navbar/navbar.jsx
+++ src/js/component/navbar/navbar.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function NavBar() {
     return (
 
-        <nav class="navbar navbar-expand-lg bg-dark">
+        <nav className="navbar navbar-expand-lg bg-dark">
             <div className="container-fluid">
                 <a className="navbar-brand text-white" href="#">Start Bootstrap</a>
                 {/* BOTON MENU HAMBURGUESA */}
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler bg-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-
+                {/*elementos que estan dentro de la barra de navegacion*/}
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li class="nav-item">
